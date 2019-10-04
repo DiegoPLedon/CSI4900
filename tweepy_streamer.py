65bf8c9d06a50436d79ee00001d41ea1e3c5d3df
HiddenSquid <diego.pledon@gmail.com>
2019-10-04 01:24:54 -0400

    Added Kafka functionality
    Commented out Tweepy stand alone method. StdOutListener handles JSON file and data printing for now

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -1,43 +1,42 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+from kafka import SimpleProducer, KafkaClient
 
 import twitter_credentials
 
-
+"""
 #Twitter Stream
 class TwitterStreamer():
-    """
-    Class for streaming and processing live tweets.
-    """
+    Class for streaming and processing live tweets
+    
 
     def __init__(self):
         pass
 
-    def stream_tweets(self, fetched_tweets, hash_tag_list):
-        # This handles Twitter authetification and the connection to Twitter Streaming API
-        listener = StdOutListener(fetched_tweets)
-        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_KEY_SECRET)
-        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
-        stream = Stream(auth, listener)
+    def stream_tweets(self, fetched_tweets_file, hash_tag_list):
+
 
         # This line filter Twitter Streams to capture data by the keywords:
         stream.filter(track=hash_tag_list)
-
+"""
 
 #Stream Listener
 class StdOutListener(StreamListener):
     """
-    Basic listener that prints out tweet data
+    Prints out data with stdOut
+    Sends tweet data to Kafka server
+    Appends tweet data ti JSON File
     """
 
-    def __init__(self, fetched_tweets):
-        self.fetched_tweets = fetched_tweets
+    def __init__(self, fetched_tweets_file):
+        self.fetched_tweets_file = fetched_tweets_file
 
     def on_data(self, data):
+        producer.send_messages("Ottawa", data.encode('utf-8'))
         try:
             print(data)
-            with open(self.fetched_tweets, 'a') as tf:
+            with open(self.fetched_tweets_file, 'a') as tf:
                 tf.write(data)
             return True
         except BaseException as e:
@@ -49,9 +48,17 @@ class StdOutListener(StreamListener):
 
 
 if __name__ == '__main__':
-    # Authenticate using config.py and connect to Twitter Streaming API.
-    hash_tag_list = ["diego", "politics", "Justyn Trudeau"]
-    fetched_tweets = "tweets.json"
+    hash_tag_list = ["ottawa"]
+    fetched_tweets_file = "tweets.json"
 
-    twitter_streamer = TwitterStreamer()
-    twitter_streamer.stream_tweets(fetched_tweets, hash_tag_list)
+    kafka = KafkaClient("localhost:9092")
+    producer = SimpleProducer(kafka)
+    listener = StdOutListener(fetched_tweets_file)
+
+    auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_KEY_SECRET)
+    auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+    stream = Stream(auth, listener)
+    stream.filter(track=hash_tag_list)
+
+    #twitter_streamer = TwitterStreamer()
+    #twitter_streamer.stream_tweets(fetched_tweets_file, hash_tag_list)
