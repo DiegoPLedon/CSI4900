6127ef0ee8967d3bea6308cfe7c8ae4d90a0d4e4
HiddenSquid <diego.pledon@gmail.com>
2019-11-30 17:20:16 -0500

    Added hashtag nodes and relationship

--- uplodtweets.py
+++ uplodtweets.py
@@ -1,11 +1,10 @@
-
 import json
 from py2neo import Graph, Node, Relationship
 
 # B
 from kafka import KafkaConsumer
 
-graph = Graph("")
+graph = Graph("bolt://neo4j:1Test123@ec2-3-212-83-143.compute-1.amazonaws.com:7687/db/data/")
 
 graph.run("CREATE CONSTRAINT ON (u:User) ASSERT u.username IS UNIQUE")
 graph.run("CREATE CONSTRAINT ON (t:Tweet) ASSERT t.id IS UNIQUE")
@@ -17,7 +16,12 @@ def add_user(user):
     graph.create(temp)
 
 
-def relate(user1, type, user2):
+def add_hashtag(hashtag):
+    temp = Node("Hashtag", name=hashtag["text"].lower())
+    graph.create(temp)
+
+
+def relate_users(user1, type, user2):
     user_node_1 = graph.nodes.match("User", username=user1.lower()).first()
     user_node_2 = graph.nodes.match("User", username=user2['screen_name'].lower()).first()
     if (not user_node_2):
@@ -28,18 +32,35 @@ def relate(user1, type, user2):
     graph.create(Relationship(user_node_1, type, temp))
 
 
+def relate_hashtag_to_user(user, hashtag):
+    user_node = graph.nodes.match("User", username=user["screen_name"].lower()).first()
+    if (not user_node):
+        temp = Node("User", id=user["id"], name=user["name"], username=user["screen_name"].lower())
+    print(hashtag)
+    print("Hashtag", user['screen_name'])
+    graph.create(Relationship(user_node, "Hashtag", hashtag))
+
+
 consumer = KafkaConsumer('Ottawa')
 for msg in consumer:
     msg_deserialized = json.loads(msg.value)
+    print()
     try:
         m = graph.nodes.match("User", username=msg_deserialized['user']['screen_name'].lower()).first()
     except:
         print("Rate limit hit, waiting...")
         continue
+
     if (not m):
         add_user(msg_deserialized['user'])
     for mention in msg_deserialized['entities']['user_mentions']:
-        relate(msg_deserialized['user']['screen_name'], "MENTIONS", mention)
+        relate_users(msg_deserialized['user']['screen_name'], "MENTIONS", mention)
     if ('quoted_status' in msg_deserialized and 'extended_tweet' in msg_deserialized['quoted_status']):
         for mention in msg_deserialized['quoted_status']['extended_tweet']['entities']['user_mentions']:
-            relate(msg_deserialized['user']['screen_name'], "MENTIONS", mention)
+            relate_users(msg_deserialized['user']['screen_name'], "MENTIONS", mention)
+    for hashtag in msg_deserialized['entities']['hashtags']:
+        hashtag_node = graph.nodes.match("Hashtag", name=hashtag['text'].lower()).first()
+        if (not hashtag_node):
+            add_hashtag(hashtag)
+        hashtag_node = graph.nodes.match("Hashtag", name=hashtag['text'].lower()).first()
+        relate_hashtag_to_user(msg_deserialized['user'], hashtag_node)
