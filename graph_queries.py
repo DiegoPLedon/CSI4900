dd6c36bc38423fbf961d35359ffe16d98b45cb9c
HiddenSquid <diego.pledon@gmail.com>
2019-12-11 23:13:39 -0500

    Added last of queries

--- graph_queries.py
+++ graph_queries.py
@@ -50,16 +50,47 @@ def most_used_hashtags(graph):
     for d in data:
         print(d)
 
+def louvain_algorithm(graph):
+    query = """
+    CALL algo.louvain.stream('User', 'MENTIONS', {})
+    YIELD nodeId, community
+
+    RETURN algo.asNode(nodeId).name AS user, community
+    ORDER BY community DESC;
+    """
+
+    data = graph.run(query)
+    for d in data:
+        print(d)
+
+def betweennes_connected_algorithm(graph):
+    query = """
+    CALL algo.betweenness.stream('User','MENTIONS',{direction:'in'})
+    YIELD nodeId, centrality
+
+    MATCH (user:User) WHERE id(user) = nodeId
+
+    RETURN user.username AS username,centrality
+    ORDER BY centrality DESC
+        """
+
+    data = graph.run(query)
+    for d in data:
+        print(d)
+
 def choice():
     program_running = True
     while program_running:
         print("These are the possible actions for the graph: ")
         print("0: Exit from program")
-        print("1: Retrieve the nodes that are in a loop")
-        print("2: Delete all nodes and relationships from graph")
+        print("1: Delete all nodes and relationships from graph")
+        print("2: Retrieve the nodes that are in a loop")
         print("3: Return the Page rank of the top 10 Nodes by running the Page score algorithm")
         print("4: Return the Nodes with the most connections")
         print("5: Return the most used Hashtags")
+        print("6: Louvain Community detection algorithm")
+        print("7: Betweenness community algorithm ")
+
         inp = input("Enter the number for the action to be done on the graph: ")
         print("")
         if inp == "0":
@@ -77,12 +108,20 @@ def choice():
             page_rank(graph)
             print("")
         if inp == "4":
-            print("Returning the most used Hashtags: \n")
+            print("Returning the most connected nodes: \n")
             most_connected_nodes(graph)
             print("")
         if inp == "5":
             print("Returning the most used Hashtags: \n")
             most_used_hashtags(graph)
+            print("")
+        if inp == "6":
+            print("Running the Louvain algorithm: \n")
+            louvain_algorithm(graph)
+            print("")
+        if inp == "7":
+            print("Running Betweenness Centrality algorithm: \n")
+            betweennes_connected_algorithm(graph)
             print("")
         else:
             print("Input not valid, please type the number of the option you want, and click enter\n")
